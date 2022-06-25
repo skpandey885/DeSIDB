6f92497b019205078b7066fc6abfddbc7a8df7d9
Sachin Pandey <skpandey885@gmail.com>
2022-06-25 20:01:04 +0530

    College Resitration form half part completed

--- src/pages/register/RegisterCollegePage.js
+++ src/pages/register/RegisterCollegePage.js
@@ -2,7 +2,93 @@ import React from 'react'
 
 const RegisterCollegePage = () => {
   return (
-    <div>RegisterCollegePage</div>
+  
+    <div class="flow-root ">
+     <div className="float-left md:pt-[125px] pl-8 register-sign">
+     <img src="/assets/register.png" class="max-w-full h-auto" alt="..." />
+     </div> 
+
+   <div className="flex items-end justify-end p-20">
+  <div className="mx-auto w-full max-w-[550px]">
+    <form action="https://formbold.com/s/FORM_ID" method="POST">
+      <div className="mb-5">
+        <label
+          htmlFor="name"
+          className="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Full Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Full Name"
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Email Address
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="example@domain.com"
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="subject"
+          className="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Subject
+        </label>
+        <input
+          type="text"
+          name="subject"
+          id="subject"
+          placeholder="Enter your subject"
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="message"
+          className="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Message
+        </label>
+        <textarea
+          rows={4}
+          name="message"
+          id="message"
+          placeholder="Type your message"
+          className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+          defaultValue={""}
+        />
+      </div>
+      <div>
+        <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none">
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+    </div>
   )
 }
 
