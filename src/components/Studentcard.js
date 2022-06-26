50a8a3bf5da70b035b9539ba467386d98dcbc7b8
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-06-26 11:40:38 +0530

    fixed error and added filter

--- src/components/Studentcard.js
+++ src/components/Studentcard.js
@@ -5,7 +5,7 @@ const Studentcard = ({ data }) => {
     return (
         <div>
             <Link to="/browse/students/studentDetail">
-                <div className="p-5  min-w-[320px] bg-white rounded-lg border shadow-md sm:p-8  ">
+                <div className="p-5  w-[320px] bg-white rounded-lg border shadow-md sm:p-8  ">
                     <div className="flex items-baseline text-gray-900 ">
                         <span className="text-3xl font-extrabold tracking-tight"> {data.firstName +' ' +  data.lastName} </span>
                     </div>
@@ -54,14 +54,12 @@ const Studentcard = ({ data }) => {
                                 Gender : <span className='text-lg font-normal '>{data.gender}</span>
                             </span>
                         </li>
-
-
                     </ul>
                     <button
                     type="button"
                     className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
                 >
-                    <a href='https://soa.ac.in'>View Details    </a>
+                    View Details
                 </button>
                 </div>
             </Link>
