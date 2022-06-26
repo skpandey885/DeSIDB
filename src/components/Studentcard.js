e6f666e24116457b6250c25134d0c6c7f0bab683
mehulpuri <lonewolfbs1@gmail.com>
2022-06-26 09:25:57 +0530

    fixes

--- src/components/Studentcard.js
+++ src/components/Studentcard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BsArrowRight } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
-const Studentcard = () => {
+const Studentcard = ({data}) => {
     return (
         <div>
         <Link to="/browse/students/studentDetail">
-        <div className="p- mb-20 max-w-sm bg-white rounded-lg border shadow-md sm:p-8 ml-5 ">
+        <div className="p-2 mb-20 max-w-sm bg-white rounded-lg border shadow-md sm:p-8 ml-5 min-w-[300px] ">
                 
                 <div className="flex items-baseline text-gray-900 ">
-                    <span className="text-4xl font-extrabold tracking-tight">Name</span>
+                    <span className="text-4xl font-extrabold tracking-tight">{data.firstName}</span>
                 </div>
                 {/* List */}
                 <ul role="list" className="my-7 space-y-5">
@@ -16,21 +16,21 @@ const Studentcard = () => {
                         {/* Icon */}
                         
                         <span className="text-xl font-bold leading-tight text-blue  ">
-                            UID : <span className='text-lg font-normal'>{'1000'}</span>
+                            UID : <span className='text-lg font-normal'>{data.sid.toString()}</span>
                         </span>
                     </li>
                     <li className="flex space-x-3">
                         {/* Icon */}
                         
                         <span className="text-xl font-bold leading-tight text-blue  ">
-                            Course : <span className='text-lg font-normal '>{'Btech '}</span>
+                            Course : <span className='text-lg font-normal '>{data.course}</span>
                         </span>
                     </li>
                     <li className="flex space-x-3">
                         {/* Icon */}
                         
                         <span className="text-xl font-bold leading-tight text-blue  ">
-                            DOB : <span className='text-lg font-normal '>{'22/04/2000 '}</span>
+                            DOB : <span className='text-lg font-normal '>{data.dateOfBirth}</span>
                         </span>
                     </li>
                    
@@ -38,21 +38,21 @@ const Studentcard = () => {
                         {/* Icon */}
                         
                         <span className="text-xl font-bold leading-tight text-blue  ">
-                            Mobile : <span className='text-lg font-normal '>{'9873647283'}</span>
+                            Mobile : <span className='text-lg font-normal '>{data.mobileNumber}</span>
                         </span>
                     </li>
                     <li className="flex space-x-3">
                         {/* Icon */}
                         
                         <span className="text-xl font-bold leading-tight text-blue  ">
-                            Email : <span className='text-lg font-normal '>{'Yes '}</span>
+                            Email : <span className='text-lg font-normal '>{data.emailId}</span>
                         </span>
                     </li>
                     <li className="flex space-x-3">
                         {/* Icon */}
                         
                         <span className="text-xl font-bold leading-tight text-blue  ">
-                            Gender : <span className='text-lg font-normal '>{'Male'}</span>
+                            Gender : <span className='text-lg font-normal '>{data.gender}</span>
                         </span>
                     </li>
 
--- src/pages/browse/BrowseStudentPage.js
+++ src/pages/browse/BrowseStudentPage.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useContract, useSigner } from 'wagmi'
 import Studentcard from '../../components/Studentcard'
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from '../../components/contract/contract';
+import Loader from '../../components/Loader';
 
 const BrowseStudentPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [allStudents, setAllStudents] = useState([]);
+  const { data: signer } = useSigner();
+
+  const contract = useContract({
+    addressOrName: CONTRACT_ADDRESS,
+    contractInterface: CONTRACT_ABI,
+    signerOrProvider: signer
+  })
+
+  const getAllStudents = async () => {
+    setLoading(true);
+    try {
+      const _data = await contract.getAllStudents();
+      console.log(_data);
+      setAllStudents(_data);
+    } catch (err) {
+      console.log(err)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (signer) {
+      getAllStudents();
+    }
+  }, [signer])
+
+
+  if (!signer) {
+    return <div className='h-[90vh] w-screen flex items-center justify-center'>Pleae Connect to your metamask wallet</div>
+  }
+
+  if (loading) {
+    return <div className='h-[90vh] w-screen flex items-center justify-center'><Loader /></div>
+  }
+
   return (
-    <div>
-      <Studentcard />
+    <div className='min-h-screen px-6 py-4 '>
+      <h1 className='text-5xl ml-5 mb-20 font-semibold '>Registered Students</h1>
+      <div className='ml-5'>
+        <div className="flex gap-6 flex-wrap -m-2">
+          {allStudents.map((data) => (
+            <Studentcard data={data} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
