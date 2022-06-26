4dd4f8360ed4b98858b3ce55191d32fc748e3ce2
Sachin Pandey <skpandey885@gmail.com>
2022-06-26 12:36:17 +0530

    New Changes

--- src/pages/register/RegisterStudentPage.js
+++ src/pages/register/RegisterStudentPage.js
@@ -29,11 +29,11 @@ const Student = () => {
   return (
     <div>
         <section className="h-auto dark:bg-main-dark-bg dark:text-white">
-          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 text-center">
-            <p className="mt-1 text-4xl font-extrabold  text-gray-900 sm:text-5xl sm:tracking-tight">
+          <div className="px-4 py-16 mx-auto text-center max-w-7xl sm:py-24 sm:px-6 lg:px-8">
+            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight">
               Register Yourself and Get an early start on your Career
             </p>
-            <p className="max-w-3xl mt-5 mx-auto text-xl text-gray-500">
+            <p className="max-w-3xl mx-auto mt-5 text-xl text-gray-500">
               Registering here makes you visible to Nation's top colleges and might help you get your dream college.
             </p>
             <StudentForm />
