d761a0f44dfdca3fb66fc6b6d28adc68c2393b7b
mehulpuri <lonewolfbs1@gmail.com>
2022-07-04 20:51:42 +0530

    update page

--- frontend/src/pages/UpdateStudentDetail.js
+++ frontend/src/pages/UpdateStudentDetail.js
@@ -30,7 +30,7 @@ const UpdateStudentDetail = () => {
   const [date, setDate] = useState();
   const [data, setData] = useState();
   const {register,handleSubmit,setValue} = useForm()
-  const [submitStatus, updateSubmit] = useState("Verify");
+  const [submitStatus, updateSubmit] = useState("Update");
   const [currClass, updateClass] = useState("text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2");
   const [disabledStatus, disabledUpdate] = useState(false);
 
