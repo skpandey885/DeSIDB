1816517c8a86c276a90f70e50a64a0495812f664
Sachin Pandey <78532081+skpandey885@users.noreply.github.com>
2022-07-07 20:40:43 +0530

    Update RegisterStudentPage.js

--- frontend/src/pages/register/RegisterStudentPage.js
+++ frontend/src/pages/register/RegisterStudentPage.js
@@ -77,10 +77,6 @@ const Student = () => {
 
   }
 
-  if (!signer) {
-    return <div className='h-[90vh] w-screen flex items-center justify-center'>Please Connect to your metamask wallet</div>
-  }
-
   return (
     <div>
       <section className="h-auto px-12 pb-12">
